26980fe2fce4c1b58e70f63d29cf156fd309c659
pjmcveyroutalk <pjmcvey.routalk@gmail.com>
2026-08-14 18:42:07 -0500

    Remove mobileFix stylesheet injection

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-const mobileFix = document.createElement("link");
-
-mobileFix.rel = "stylesheet";
-mobileFix.href = "mobile-fix.css";
-
-document.head.appendChild(mobileFix);
-
 document.getElementById("year").textContent =
   new Date().getFullYear();
