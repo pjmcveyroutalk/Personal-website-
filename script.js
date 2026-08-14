ded22683b59512342d74a70143eb6b073bc101d7
pjmcveyroutalk <pjmcvey.routalk@gmail.com>
2026-08-14 18:31:34 -0500

    Add mobile stylesheet link and update year display

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById("year").textContent = new Date().getFullYear();
+const mobileFix = document.createElement("link");
+
+mobileFix.rel = "stylesheet";
+mobileFix.href = "mobile-fix.css";
+
+document.head.appendChild(mobileFix);
+
+document.getElementById("year").textContent =
+  new Date().getFullYear();
